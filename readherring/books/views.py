282ed4d98663b04b99dc362787dd3788b73beafb
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-07-14 22:53:42 +0100

    add show all books route

--- readherring/books/views.py
+++ readherring/books/views.py
@@ -1,24 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from bson.json_util import loads, dumps
 
-# Create your views here.
-def index(request):
-    return HttpResponse("<h1>Hello and welcome to my first <u>Django App</u> project!</h1>")
-
-# from pymongo import MongoClient
-# client = pymongo.MongoClient('connection_string')
-# db = client['db_name']
 
 import pymongo
-# from django.conf import settings
 my_client = pymongo.MongoClient('mongodb+srv://readherring:readherring@readherring.qlngl1v.mongodb.net/?retryWrites=true&w=majority')
 
-# First define the database name
 dbname = my_client['readherring']
 
-# Now get/create collection name (remember that you will see the database in your mongodb cluster only after you create a collection)
 collection_name = dbname['Books']
 
+collection_name.drop({})
 #let's create two documents
 book1 = {
     "title": "first book",
@@ -29,14 +21,27 @@ book2 = {
     "author" : "second author",
 }
 
-print('hello ******************')
-
 collection_name.insert_many([book1, book2])
 
 books_list = collection_name.find({})
 
-for r in books_list:
-	print(r['title'])
+# for r in books_list:
+# 	print(r)
+# print(dumps(books_list))
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Hello and welcome to my first <u>Django App</u> project!</h1>")
+
+def getall(request):
+    book_list = []
+    data = collection_name.find({})
+    for book in data:
+        book['id'] = str(book['_id'])
+        book.pop('_id', None)
+        book_list.append(book)
+    return JsonResponse(book_list, safe=False)
+
 
 # update_data = collection_name.update_one({'medicine_id':'RR000123456'}, {'$set':{'common_name':'Paracetamol 500'}})
 
