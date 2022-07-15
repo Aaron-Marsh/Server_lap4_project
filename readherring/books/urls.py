cfb47efbf0d5d68bab70ccc00017f3ec244c6a31
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-07-15 11:11:55 +0100

    add get by ISBN number capability

--- readherring/books/urls.py
+++ readherring/books/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('books/', views.getall, name='get'),
+    path('books/', views.get_all_books, name='get_all_books'),
+    path('books/<int:ISBN>', views.get_by_ISBN, name='get_by_ISBN'),
     ]
--- readherring/books/views.py
+++ readherring/books/views.py
@@ -6,19 +6,21 @@ from bson.json_util import loads, dumps
 import pymongo
 my_client = pymongo.MongoClient('mongodb+srv://readherring:readherring@readherring.qlngl1v.mongodb.net/?retryWrites=true&w=majority')
 
-dbname = my_client['readherring']
+db = my_client['readherring']
 
-collection_name = dbname['Books']
+collection_name = db['Books']
 
 collection_name.drop({})
 #let's create two documents
 book1 = {
     "title": "first book",
-    "author" : "first author"
+    "author": "first author",
+    "ISBN": "12345"
 }
 book2 = {
     "title": "second title",
     "author" : "second author",
+    "ISBN": "54321"
 }
 
 collection_name.insert_many([book1, book2])
@@ -33,7 +35,7 @@ books_list = collection_name.find({})
 def index(request):
     return HttpResponse("<h1>Hello and welcome to my first <u>Django App</u> project!</h1>")
 
-def getall(request):
+def get_all_books(request):
     book_list = []
     data = collection_name.find({})
     for book in data:
@@ -41,6 +43,14 @@ def getall(request):
         book.pop('_id', None)
         book_list.append(book)
     return JsonResponse(book_list, safe=False)
+
+def get_by_ISBN(request, ISBN):
+    ISBN_string = str(ISBN)
+    data = collection_name.find({'ISBN': ISBN_string})
+    book = data[0]
+    book['id'] = str(book['_id'])
+    book.pop('_id', None)
+    return JsonResponse(book, safe=False)
 
 
 # update_data = collection_name.update_one({'medicine_id':'RR000123456'}, {'$set':{'common_name':'Paracetamol 500'}})
