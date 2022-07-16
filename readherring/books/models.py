d0aa26a595028c3d9aea080e6c6bd9cd2aa127e0
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-07-16 21:02:38 +0100

    add api call for books and process books data to send to client

--- readherring/books/models.py
+++ readherring/books/models.py
@@ -5,7 +5,6 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     ISBN = models.CharField(max_length=200, default='isbn')
-    reviews = []
     rating = models.IntegerField(default=0)
     num_ratings = models.IntegerField(default=0)
 
