0e1ad79392d7b818d9006dfb17376d90b16a06e2
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-07-17 21:30:02 +0100

    start creating models and views for users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Login(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=10)
+    class Meta:
+        db_table="Login"
