545b866572d5023257b8ba742f8ea11988f974cf
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-07-17 03:32:34 +0100

    add forum layout

--- forums/views.py
+++ forums/views.py
@@ -13,16 +13,103 @@ db = my_client['readherring']
 collection_name = db['Forums']
 
 collection_name.drop({})
-#let's create two documents
+
 thread1 = {
-    "title": "first title",
-    "username": "user 1",
-    "first_message": "message 1"
+    "title": "Hello Chris! This is the title of a thread",
+    "username": "Aaron, the creator of this thread",
+    "first_message": "The first main message that starts the thread off goes here. Let me know if anything needs to be restructured to make it easier to display/ function, or if you want to display it a different way!",
+    "messages": [{"message_id": "id I will generate for you to be referenced in replies", "username": "user who sent this message", "message": "Whatever could this be?",
+    "replies": [
+        {
+        "message_id": "id I will generate for you to be referenced in replies",
+        "username": "user who sent this reply",
+        "reply": "The first reply to this message",
+        "reply_to": "Is this a reply to another user who has replied or is it just a reply to the original message in which case ''"
+        },
+        {
+        "message_id": "id I will generate for you to be referenced in replies",
+        "username": "user",
+        "reply": "The second reply to this message",
+        "reply_to": ""
+        }
+    ]},
+    {"message_id": 12345, "username": "user1", "message": "user1 has sent another message to view in the thread",
+    "replies": [
+        {
+        "message_id": 12345,
+        "username": "user2",
+        "reply": "user2 has sent this reply to the main message from user1",
+        "reply_to": ""
+        },
+        {
+        "message_id": 12345,
+        "username": "user3",
+        "reply": "user3 has also sent this reply to the main message",
+        "reply_to": ""
+        },
+        {
+        "message_id": 12345,
+        "username": "user1",
+        "reply": "user1 has replied to user2's message",
+        "reply_to": "user2"
+        },
+        {
+        "message_id": 12345,
+        "username": "user4",
+        "reply": "user4 has replied to user3's message",
+        "reply_to": "user3"
+        },
+        {
+        "message_id": 12345,
+        "username": "user5",
+        "reply": "user5 has sent another reply to the original message not targeted at anyone",
+        "reply_to": ""
+        }
+        
+    ]}
+    ]
 }
 thread2 = {
-    "title": "second title",
-    "username": "user 2",
-    "first_message": "message 2"
+    "title": "A second thread",
+    "username": "user1",
+    "first_message": "What an original message this is",
+    "messages": [{"message_id": 11111, "username": "user2", "message": "This is message 1",
+    "replies": [
+        {
+        "message_id": 11111,
+        "username": "user3",
+        "reply": "The first reply to message 1",
+        "reply_to": ""
+        },
+        {
+        "message_id": 11111,
+        "username": "user4",
+        "reply": "The second reply to message 1",
+        "reply_to": ""
+        }
+    ]},
+    {"message_id": 22222, "username": "user5", "message": "This is message 2",
+    "replies": [
+        {
+        "message_id": 22222,
+        "username": "user1",
+        "reply": "The first reply to message 2",
+        "reply_to": ""
+        },
+        {
+        "message_id": 22222,
+        "username": "user3",
+        "reply": "The second reply to message 2 which is also a reply to user1",
+        "reply_to": "user1"
+        },
+        {
+        "message_id": 22222,
+        "username": "user5",
+        "reply": "Third reply to message 2",
+        "reply_to": ""
+        }
+    ]}
+    ]
 }
 
 collection_name.insert_many([thread1, thread2])
@@ -64,7 +151,6 @@ def get_by_id(request, id):
             username = json_data['username']
             message = json_data['message']
             message_id = str(uuid.uuid4())
-            print(message_id)
             collection_name.update_one({'_id': ObjectId(id)},{'$push':{'messages': {'message_id': message_id, 'username': username, 'message': message, 'replies': [] }}}, upsert=True)
             return HttpResponse('New Message Added To Thread!')
         elif json_data['method'] == 'reply_message':
