193002b5011bfe31e6f1dd3481f33f3b60a33abe
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-07-18 21:46:09 +0100

    move forum seeds to seperate file

---
+++ forums/seeds.py
@@ -0,0 +1,86 @@
+
+thread1 = {
+    "title": "Hello Chris! This is the title of a thread",
+    "username": "Aaron, the creator of this thread",
+    "first_message": "The first main message that starts the thread off goes here. Let me know if anything needs to be restructured to make it easier to display/ function, or if you want to display it a different way!",
+    "messages": [{"message_id": "id I will generate for you to be referenced in replies", "username": "user who sent this message", "message": "Whatever could this be?",
+    "replies": [
+        {
+        "username": "user who sent this reply",
+        "reply": "The first reply to this message",
+        "reply_to": "Is this a reply to another user who has replied or is it just a reply to the original message in which case ''"
+        },
+        {
+        "username": "user",
+        "reply": "The second reply to this message",
+        "reply_to": ""
+        }
+    ]},
+    {"message_id": '12345', "username": "user1", "message": "user1 has sent another message to view in the thread",
+    "replies": [
+        {
+        "username": "user2",
+        "reply": "user2 has sent this reply to the main message from user1",
+        "reply_to": ""
+        },
+        {
+        "username": "user3",
+        "reply": "user3 has also sent this reply to the main message",
+        "reply_to": ""
+        },
+        {
+        "username": "user1",
+        "reply": "user1 has replied to user2's message",
+        "reply_to": "user2"
+        },
+        {
+        "username": "user4",
+        "reply": "user4 has replied to user3's message",
+        "reply_to": "user3"
+        },
+        {
+        "username": "user5",
+        "reply": "user5 has sent another reply to the original message not targeted at anyone",
+        "reply_to": ""
+        }
+        
+    ]}
+    ]
+}
+thread2 = {
+    "title": "A second thread",
+    "username": "user1",
+    "first_message": "What an original message this is",
+    "messages": [{"message_id": '11111', "username": "user2", "message": "This is message 1",
+    "replies": [
+        {
+        "username": "user3",
+        "reply": "The first reply to message 1",
+        "reply_to": ""
+        },
+        {
+        "username": "user4",
+        "reply": "The second reply to message 1",
+        "reply_to": ""
+        }
+    ]},
+    {"message_id": '22222', "username": "user5", "message": "This is message 2",
+    "replies": [
+        {
+        "username": "user1",
+        "reply": "The first reply to message 2",
+        "reply_to": ""
+        },
+        {
+        "username": "user3",
+        "reply": "The second reply to message 2 which is also a reply to user1",
+        "reply_to": "user1"
+        },
+        {
+        "username": "user5",
+        "reply": "Third reply to message 2",
+        "reply_to": ""
+        }
+    ]}
+    ]
+}
--- forums/views.py
+++ forums/views.py
@@ -4,103 +4,23 @@ from bson.json_util import loads, dumps
 import json
 from bson.objectid import ObjectId
 import uuid;
-
 import pymongo
+
+# For refreshing server to seed data
+from forums.seeds import thread1, thread2
+# collection_name.drop({})
+# collection_name.insert_many([thread1, thread2])
+
+
+
 my_client = pymongo.MongoClient('mongodb+srv://readherring:readherring@readherring.qlngl1v.mongodb.net/?retryWrites=true&w=majority')
 
 db = my_client['readherring']
 
 collection_name = db['Forums']
 
-# collection_name.drop({})
 
-thread1 = {
-    "title": "Hello Chris! This is the title of a thread",
-    "username": "Aaron, the creator of this thread",
-    "first_message": "The first main message that starts the thread off goes here. Let me know if anything needs to be restructured to make it easier to display/ function, or if you want to display it a different way!",
-    "messages": [{"message_id": "id I will generate for you to be referenced in replies", "username": "user who sent this message", "message": "Whatever could this be?",
-    "replies": [
-        {
-        "username": "user who sent this reply",
-        "reply": "The first reply to this message",
-        "reply_to": "Is this a reply to another user who has replied or is it just a reply to the original message in which case ''"
-        },
-        {
-        "username": "user",
-        "reply": "The second reply to this message",
-        "reply_to": ""
-        }
-    ]},
-    {"message_id": '12345', "username": "user1", "message": "user1 has sent another message to view in the thread",
-    "replies": [
-        {
-        "username": "user2",
-        "reply": "user2 has sent this reply to the main message from user1",
-        "reply_to": ""
-        },
-        {
-        "username": "user3",
-        "reply": "user3 has also sent this reply to the main message",
-        "reply_to": ""
-        },
-        {
-        "username": "user1",
-        "reply": "user1 has replied to user2's message",
-        "reply_to": "user2"
-        },
-        {
-        "username": "user4",
-        "reply": "user4 has replied to user3's message",
-        "reply_to": "user3"
-        },
-        {
-        "username": "user5",
-        "reply": "user5 has sent another reply to the original message not targeted at anyone",
-        "reply_to": ""
-        }
-        
-    ]}
-    ]
-}
-thread2 = {
-    "title": "A second thread",
-    "username": "user1",
-    "first_message": "What an original message this is",
-    "messages": [{"message_id": '11111', "username": "user2", "message": "This is message 1",
-    "replies": [
-        {
-        "username": "user3",
-        "reply": "The first reply to message 1",
-        "reply_to": ""
-        },
-        {
-        "username": "user4",
-        "reply": "The second reply to message 1",
-        "reply_to": ""
-        }
-    ]},
-    {"message_id": '22222', "username": "user5", "message": "This is message 2",
-    "replies": [
-        {
-        "username": "user1",
-        "reply": "The first reply to message 2",
-        "reply_to": ""
-        },
-        {
-        "username": "user3",
-        "reply": "The second reply to message 2 which is also a reply to user1",
-        "reply_to": "user1"
-        },
-        {
-        "username": "user5",
-        "reply": "Third reply to message 2",
-        "reply_to": ""
-        }
-    ]}
-    ]
-}
 
-# collection_name.insert_many([thread1, thread2])
 
 # Create your views here.
 def get_create_threads(request):
