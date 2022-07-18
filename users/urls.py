9d442b2d91281f77c6cefe1319170ff49abc3304
Izgardon <williamsessions23@gmail.com>
2022-07-18 10:25:56 +0100

    added new login urls

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('users/', views.get_users, name='get_users'),
     path('users/<str:username>', views.get_by_username, name='get_by_username'),
+    path('login', views.user_login),
+    path('logout', views.user_logout),
+    path('register', views.new_user)
     ]
