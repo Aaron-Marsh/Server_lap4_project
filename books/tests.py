5e0871f8ebfbd2e08f09cd0f83034362a934bc1e
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-07-19 02:12:21 +0100

    add url tests for books routes

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestUrls(TestCase):
+
+    def testHomePage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def testBooksPage(self):
+        response = self.client.get('/books/')
+        self.assertEqual(response.status_code, 200)
+
+    def testBooksISBNPage(self):
+        response = self.client.get('/books/123')
+        self.assertEqual(response.status_code, 200)
+
+    def testBooksApiPage(self):
+        response = self.client.get('/books/api/')
+        self.assertEqual(response.status_code, 200)
