eef5d1ae83e30ca92bc7c45164068ffbbe724349
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-07-21 17:19:21 +0100

    change isbn from int to str

--- books/urls.py
+++ books/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.get_create_books, name='get_create_books'),
-    path('books/<int:ISBN>', views.get_by_ISBN, name='get_by_ISBN'),
+    path('books/<str:ISBN>', views.get_by_ISBN, name='get_by_ISBN'),
     path('books/api/', views.get_books_from_api, name='get_books_from_api'),
     ]
